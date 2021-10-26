6f2fc349ba12c372e06a2b93174e58915c03a4a2
ekballo <81698678+ekballo@users.noreply.github.com>
2021-10-26 12:41:15 -0300

    Aula02/Form

---
+++ php/Projeto_01/exemplos/Aula02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario</title>
+</head>
+<body>
+    <?php
+        if (isset($_POST['acao'])){
+            $nome = @$_POST['nome'];
+            $email =@$_POST['email'];
+            echo $nome;
+            echo '<br />';
+            echo $email;
+        }
+    ?>
+    <form method="post">
+        <input type="text" name="nome" />
+        <input type="text" name="email" />
+        <input type="submit" name="acao" value="Enviar" />
+    </form>
+</body>
+</html>
