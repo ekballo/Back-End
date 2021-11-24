7978d67cba119d74ee0bac207875a6a125c5615c
ekballo <81698678+ekballo@users.noreply.github.com>
2021-11-24 13:28:22 -0300

    Exercicio02

---
+++ Python/EstruturaSequencial/Exercicio02.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+num = float(input('Digite um número: '))
+print('O número digitado foi {:.1f} '.format(num))
