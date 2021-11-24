1154923c47732e1aede50eb05ae6e418982ac674
ekballo <81698678+ekballo@users.noreply.github.com>
2021-11-24 13:32:20 -0300

    Exercicios

---
+++ Python/EstruturaSequencial/Exercicios/Exercicio03.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima a soma.
+num1 = int(input('Digite um Numero: '))
+num2 = int(input('Digite um Numero: '))
+
+soma = num1 + num2
+
+print('A soma dos dois valores digitado são {}'.format(soma,num1,num2))
