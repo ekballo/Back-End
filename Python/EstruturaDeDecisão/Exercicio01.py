66b0e40683722c656d5cf2c07df4a281d67850bd
ekballo <81698678+ekballo@users.noreply.github.com>
2021-11-25 22:22:50 -0300

    Exercicio01

---
+++ Python/EstruturaDeDecisão/Exercicio01.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+
+n1 = int(input('Digite o Primeiro Numero: '))
+n2 = int(input('Digite o Segundo Numero: '))
+
+if n1 > n2:
+    print('O número digito é o maior {}'.format(n1,n2))
+else:
+    print('FIM')
