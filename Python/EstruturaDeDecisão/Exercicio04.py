cc165ca3b481bea1a403b7679ec39b4e15141617
ekballo <81698678+ekballo@users.noreply.github.com>
2021-11-29 23:25:32 -0300

    Exercicio05

---
+++ Python/EstruturaDeDecisão/Exercicio04.py
@@ -0,0 +1,15 @@
+letra = str(input('Digite uma letra em (minuscula) : '))
+
+if letra == 'a':
+    print('Vogal')
+elif letra == 'e':
+    print('Vogal')
+elif letra == 'i':
+    print('Vogal')
+elif letra == 'o':
+    print('Vogal')
+elif letra == 'u':
+    print('Vogal')
+    print('Insira uma letra ou consoante')
+else:
+    print('Consoante')
