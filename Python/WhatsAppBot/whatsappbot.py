ddeb951c90eaa7fdfeadb1a36b6a19b35d7c07b9
ekballo <81698678+ekballo@users.noreply.github.com>
2022-01-13 14:42:26 -0300

    Delete Python/WhatsAppBot directory
    foi descontiuado os comandos

--- Python/WhatsAppBot/whatsappbot.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-import time
-from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://web.whatsapp.com/')
-time.sleep(30)
-contato = ['','Vanessa']
